6b7e73d9bf437ef6a4b6eb66477af65abdf2adf9
Elena <elenaharan@gmail.com>
2022-06-07 22:34:42 +0100

    added custom shell component Card

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem =(expenses) => {
   
   return(
-  <div className='expense-item'>
+    <Card className='expense-item'>
     <ExpenseDate date = {expenses.date}/>  
     <div className='expense-item__description'>
       <h2>{expenses.title}</h2>
       <div className='expense-item__price'>£{expenses.amount}</div>
     </div>
-  </div>
+  </Card>
   )    
   
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,20 @@
 import ExpenseItem from './ExpenseItem';
-import './Expenses.css'
+import Card from '../UI/Card';
 
-function Expenses (props) {
+const Expenses = (props) => {
   
 return(
-  <div className='expenses'>
+  <Card className='expenses'>
     <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount} />
     <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount} />
     <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount} />
     <ExpenseItem date={props.items[3].date} title={props.items[3].title} amount={props.items[3].amount} />
-  </div>
+  </Card>
 )
 }
 
 export default Expenses;
+
+<h1>
+  
+ </h1>
